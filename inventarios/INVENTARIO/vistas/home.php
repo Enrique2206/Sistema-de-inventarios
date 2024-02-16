87da75dc29a4185464ec24dc44ee4271caaef833
Denis Enrique Salazar Jimenez <104470561+Enrique2206@users.noreply.github.com>
2024-02-16 13:22:11 -0800

    Add files via upload

---
+++ inventarios/INVENTARIO/vistas/home.php
@@ -0,0 +1,5 @@
+<div class="container is-fluid">
+
+	<h2 class="title"><center>Sistema de inventarios Ordaz Networks!</center></h2>
+	<h3 class="subtitle"><center>¡Bienvenid@ <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</center></h3>
+</div>
